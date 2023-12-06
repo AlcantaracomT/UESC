7401e5995a898f81b251c5e6cb5d4ea277abd33b
Tiago Alcantara <133592283+AlcantaracomT@users.noreply.github.com>
2023-12-06 08:58:53 -0300

    Update main.c
    Alteração de alguns comentários

--- lista_sala/IMC/main.c
+++ lista_sala/IMC/main.c
@@ -2,12 +2,12 @@
 
 #include <stdio.h>
 
-// Função que calcula o IMC de uma pessoa
+//calcula o IMC
 float calcular_imc(float peso, float altura) {
     return peso / (altura * altura);
 }
 
-// Função que imprime o status do peso de uma pessoa
+// imprime peso
 void imprimir_status(float imc) {
     if (imc < 18.5) {
         printf("Abaixo do peso.\n");
@@ -23,21 +23,16 @@ void imprimir_status(float imc) {
 int main(void) {
     float peso, altura, imc;
 
-    // Entrada: peso em quilogramas
     printf("Digite o peso em quilogramas: ");
     scanf("%f", &peso);
 
-    // Entrada: altura em metros
     printf("Digite a altura em metros: ");
     scanf("%f", &altura);
 
-    // Cálculo do IMC
     imc = calcular_imc(peso, altura);
 
-    // Saída: valor do IMC
-    printf("Seu IMC é: %.2f\n", imc);
+    printf("Seu IMC Ã©: %.2f\n", imc);
 
-    // Saída: status do peso
     imprimir_status(imc);
 
     return 0;
